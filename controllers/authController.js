c03b146e1d584f5b58dc840cfe1a2f0b12597593
Massab Iftikhar <massabiftkhar@gmail.com>
2022-08-16 22:03:21 +0500

    Initial Commit

---
+++ controllers/authController.js
@@ -0,0 +1,259 @@
+const crypto = require('crypto');
+const { promisify } = require('util');
+const jwt = require('jsonwebtoken');
+const User = require('../models/userModel');
+const catchAsync = require('../utils/catchAsync');
+const AppError = require('../utils/appError');
+const sendEmail = require('../utils/email');
+
+const signInToken = function (id) {
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.EXPIRES_IN,
+  });
+};
+
+const signInUser = (user, statuscode, res) => {
+  const token = signInToken(user._id);
+  const cookieOptions = {
+    expires: new Date(
+      Date.now() + process.env.EXPIRES_COOKIE_IN * 24 * 60 * 60 * 1000
+    ),
+
+    httpOnly: true,
+  };
+
+  if (process.env.NODE_ENV === 'production') cookieOptions.secure = true;
+
+  res.cookie('jwt', token, cookieOptions);
+
+  user.password = undefined;
+
+  res.status(statuscode).json({
+    status: 'success',
+    token,
+    data: {
+      user,
+    },
+  });
+};
+
+exports.signup = catchAsync(async (req, res, next) => {
+  const newUser = await User.create({
+    name: req.body.name,
+    email: req.body.email,
+    username: req.body.username,
+    password: req.body.password,
+    passwordConfirm: req.body.passwordConfirm,
+    passwordChangedAt: req.body.passwordChangedAt,
+    temprole: req.body.role,
+  });
+
+  signInUser(newUser._id, 201, res);
+
+  req.user = newUser;
+
+  next();
+});
+
+exports.login = catchAsync(async (req, res, next) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    // console.log('hi');
+    return next(new AppError('Email or password is not entered', 400));
+  }
+
+  const user = await User.findOne({ email }).select('+password');
+
+  if (!user || !(await user.correctPassword(password, user.password))) {
+    // console.log('hi');
+    return next(new AppError('Email or password is not correct', 401));
+  }
+
+  signInUser(user._id, 201, res);
+
+  // const token = signInToken(user._id);
+  // res.status(201).json({
+  //   status: 'success',
+  //   token,
+  // });
+});
+
+exports.protect = catchAsync(async (req, res, next) => {
+  let token;
+
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith('Bearer')
+  ) {
+    token = req.headers.authorization.split(' ')[1];
+  }
+
+  if (!token) {
+    return next(
+      new AppError('You are not logged in please login to view the data', 401)
+    );
+  }
+
+  const decoded = await promisify(jwt.verify)(token, process.env.JWT_SECRET);
+
+  const currentUser = await User.findById(decoded.id);
+
+  if (!currentUser) {
+    return next(
+      new AppError('User belonging to this token no longer exist', 401)
+    );
+  }
+
+  if (currentUser.ChangedPasswordAfter(decoded.iat)) {
+    return next(
+      new AppError('User recently changed Password please re login!')
+    );
+  }
+  req.user = currentUser;
+  req.locals.user = currentUser;
+
+  next();
+});
+
+exports.restrictTo = function (...roles) {
+  return function (req, res, next) {
+    if (!roles.includes(req.user.role)) {
+      return next(
+        new AppError('You do not have persmision to perform this action')
+      );
+    }
+    next();
+  };
+};
+
+exports.forgotPassword = catchAsync(async (req, res, next) => {
+  const user = await User.findOne({ email: req.body.email });
+
+  if (!user) {
+    return next(
+      new AppError('User with this email not found please enter valid one!')
+    );
+  }
+
+  const resetToken = user.passwordResetToken();
+
+  await user.save({ validateBeforeSave: false });
+
+  const resetURL = `${req.protocol}://${req.get(
+    'host'
+  )}/api1/resetpassword/${resetToken}`;
+
+  const message = `Forgot your password? submit patch request on the given link for the new password ${resetURL} \n If you dont do this please ignore this email`;
+
+  try {
+    await sendEmail({
+      email: user.email,
+      subject: 'Your token is valid for 10mins',
+      message,
+    });
+    res.status(200).json({
+      status: 'success',
+      message: 'Token sent to the email provided',
+    });
+  } catch (err) {
+    user.passwordResetToken = undefined;
+    user.passTokenExpire = undefined;
+    await user.save({ validateBeforeSave: false });
+
+    return next(
+      new AppError('There was error sending email please try again later!', 500)
+    );
+  }
+});
+
+exports.resetPassword = catchAsync(async (req, res, next) => {
+  const hashedToken = crypto
+    .createHash('sha256')
+    .update(req.params.token)
+    .digest('hex');
+
+  const user = await User.findOne({
+    passResetToken: hashedToken,
+    passTokenExpire: { $gt: Date.now() },
+  });
+
+  if (!user) {
+    return next(new AppError('Your token is invalid or expired', 400));
+  }
+
+  user.password = req.body.password;
+  user.passwordConfirm = req.body.passwordConfirm;
+  user.passResetToken = undefined;
+  user.passTokenExpire = undefined;
+  await user.save();
+
+  signInUser(user._id, 201, res);
+});
+
+exports.updatePass = catchAsync(async (req, res, next) => {
+  const user = await User.findById(req.user.id).select('+password');
+
+  if (!(await user.correctPassword(req.body.currentPassword, user.password))) {
+    return next(new AppError('Your current password is incorrect!', 401));
+  }
+  user.password = req.body.password;
+  user.passwordConfirm = req.body.passwordConfirm;
+  await user.save();
+
+  signInUser(user._id, 201, res);
+});
+
+exports.sendEmailConfirm = catchAsync(async (req, res, next) => {
+  const user = await User.findOne({ email: req.user.email });
+
+  if (!user) {
+    return next(new AppError('Email not found Please enter a valid one!'));
+  }
+
+  const EmailToken = user.emailResetToken();
+
+  await user.save({ validateBeforeSave: false });
+
+  const resetURL = `${req.protocol}://${req.get(
+    'host'
+  )}/api1/users/emailconfrim/${EmailToken}`;
+
+  const message = `Welcome to the Natours! Click on the given link to verify your Natours account:  ${resetURL} \n If you dont do this please ignore this email`;
+
+  try {
+    await sendEmail({
+      email: user.email,
+      subject: 'Email Confirmation',
+      message,
+    });
+  } catch (err) {
+    user.passwordResetToken = undefined;
+    user.passTokenExpire = undefined;
+    await user.save({ validateBeforeSave: false });
+
+    return next(
+      new AppError('There was error sending email please try again later!', 500)
+    );
+  }
+});
+
+exports.emailConfirm = catchAsync(async (req, res, next) => {
+  const hashedToken = crypto
+    .createHash('sha256')
+    .update(req.params.token)
+    .digest('hex');
+
+  const user = await User.findOne({
+    confirmEmailToken: hashedToken,
+  });
+  if (!user) {
+    return next(new AppError('Your token is invalid', 400));
+  }
+  user.role = user.temprole;
+  user.confirmEmailToken = undefined;
+  user.temprole = undefined;
+  await user.save({ validateBeforeSave: false });
+
+  signInUser(user._id, 201, res);
+});
